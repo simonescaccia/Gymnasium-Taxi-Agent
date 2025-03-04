118dac122fe95cbe805064c408d449cd24e4f52c
Simone Scaccia <simonescaccia.s@gmail.com>
2025-03-04 02:27:34 +0100

    update dqn

--- dqn/taxi_dqn.py
+++ dqn/taxi_dqn.py
@@ -1,10 +1,11 @@
-from collections import deque
+from collections import deque, namedtuple
 import random
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import gymnasium as gym
+import matplotlib.pyplot as plt
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -27,14 +28,21 @@ class Net(nn.Module):
 # Define memory for Experience Replay
 class ReplayMemory():
     def __init__(self, maxlen, burn_in):
-        self.memory = deque([], maxlen=maxlen)
+        self.memory = deque(maxlen=maxlen)
         self.burn_in = burn_in
-    
-    def append(self, transition):
-        self.memory.append(transition)
+        self.Buffer = namedtuple('Buffer', field_names=['state', 'action', 'reward', 'done', 'next_state'])
 
-    def sample(self, sample_size):
-        return random.sample(self.memory, sample_size)
+    
+    def sample_batch(self, batch_size=32):
+        samples = np.random.choice(len(self.memory), batch_size,
+                                   replace=False)
+        # Use asterisk operator to unpack deque
+        batch = zip(*[self.memory[i] for i in samples])
+        return batch
+
+    def append(self, s_0, a, r, d, s_1):
+        self.memory.append(
+            self.Buffer(s_0, a, r, d, s_1))
     
     # Burn-in capacity: < 1 means we need more data to start training, >= 1 means we have enough data
     def burn_in_capacity(self):
@@ -48,17 +56,23 @@ class TaxiDQN:
 
     def __init__(self):
 
+        self.env = gym.make('Taxi-v3')
+
+        self.step_count = 0
+        self.gamma = 0.99
+        self.s_0, _ = self.env.reset() # Initial state
+        self.num_states = 500
+        self.episodes = 10000
+        self.batch_size = 64
+        self.loss_function = nn.MSELoss()
+
         replay_memory_size = 5000
         replay_memory_burn_in = 1000
-        learning_rate = 0.001 
-        batch_size = 64
+        learning_rate = 0.001
         self.epsilon = 1 # 1 = 100% random actions
-        in_states = 500
+        in_states = self.num_states
         h1_nodes = 128
         out_actions = 6
-        episodes = 10000
-
-        self.env = gym.make('Taxi-v3', render_mode='human')
 
         self.policy_dqn = Net(in_states, h1_nodes, out_actions).to(device)
         self.target_dqn = Net(in_states, h1_nodes, out_actions).to(device)
@@ -71,34 +85,41 @@ class TaxiDQN:
         self.optimizer = torch.optim.Adam(self.policy_dqn.parameters(), lr=learning_rate)
 
         # List to keep track of rewards collected per episode. Initialize list to 0's.
-        self.rewards_per_episode = np.zeros(episodes)
+        self.rewards_per_episode = np.zeros(self.episodes)
 
         # List to keep track of epsilon decay
         self.epsilon_history = []
 
-        self.initialize()
-        self.step_count = 0
-        self.episode = 0
-        self.s_0, _ = self.env.reset() # Initial state
+        self.training_rewards = []
+        self.training_loss = []
+        self.update_loss = []
+        self.rewards = 0
 
+
+    def state_to_onehot(self, state):
+        
+        onehot = np.zeros(self.num_states)
+        onehot[state] = 1
+        return onehot
 
     def take_step(self, mode='exploit'):
         # choose action with epsilon greedy
         if mode == 'explore':
             action = self.env.action_space.sample()
         else:
-            action = self.network.greedy_action(torch.FloatTensor(self.s_0).to(device))
+            with torch.no_grad():
+                action = self.policy_dqn(self.state_to_onehot(self.s_0)).argmax().item()
 
         #simulate action
         s_1, r, terminated, truncated, _ = self.env.step(action)
         done = terminated or truncated
 
         #put experience in the buffer
-        self.buffer.append(self.s_0, action, r, terminated, s_1)
+        self.memory.append(self.s_0, action, r, terminated, s_1)
 
         self.rewards += r
 
-        self.s_0 = s_1.copy()
+        self.s_0 = s_1
 
         self.step_count += 1
         if done:
@@ -106,84 +127,65 @@ class TaxiDQN:
         return done
 
     # Implement DQN training algorithm
-    def train(self, gamma=0.99, max_episodes=10000,
-              network_update_frequency=10,
-              network_sync_frequency=200):
-        self.gamma = gamma
+    def train(self):
 
         # Populate replay buffer
-        while self.buffer.burn_in_capacity() < 1:
+        while self.memory.burn_in_capacity() < 1:
             self.take_step(mode='explore')
 
         ep = 0
         training = True
-        self.populate = False
         while training:
             self.s_0, _ = self.env.reset()
 
             self.rewards = 0
             done = False
             while not done:
-                if ((ep % 5) == 0):
-                    self.env.render()
+                # if ((ep % 5) == 0):
+                #     self.env.render()
 
                 p = np.random.random()
                 if p < self.epsilon:
                     done = self.take_step(mode='explore')
                 else:
                     done = self.take_step(mode='exploit')
-                # Update network
-                if self.step_count % network_update_frequency == 0:
-                    self.update()
-                # Sync networks
-                if self.step_count % network_sync_frequency == 0:
-                    self.target_network.load_state_dict(self.network.state_dict())
-                    self.sync_eps.append(ep)
+
+                self.update_network()
 
                 if done:
-                    if self.epsilon >= 0.05:
-                        self.epsilon = self.epsilon * 0.7
                     ep += 1
-                    if self.rewards > 2000:
-                        self.training_rewards.append(2000)
-                    elif self.rewards > 1000:
-                        self.training_rewards.append(1000)
-                    elif self.rewards > 500:
-                        self.training_rewards.append(500)
-                    else:
-                        self.training_rewards.append(self.rewards)
+                    
+                    # Decay epsilon
+                    self.epsilon = max(self.epsilon - 1/self.episodes, 0)
+                    self.epsilon_history.append(self.epsilon)
+                    
+                    # Log training progress
+                    self.training_rewards.append(self.rewards)
                     if len(self.update_loss) == 0:
                         self.training_loss.append(0)
                     else:
                         self.training_loss.append(np.mean(self.update_loss))
                     self.update_loss = []
-                    mean_rewards = np.mean(self.training_rewards[-self.window:])
-                    mean_loss = np.mean(self.training_loss[-self.window:])
-                    self.mean_training_rewards.append(mean_rewards)
-                    print(
-                        "\rEpisode {:d} Mean Rewards {:.2f}  Episode reward = {:.2f}   mean loss = {:.2f}\t\t".format(
-                            ep, mean_rewards, self.rewards, mean_loss), end="")
 
-                    if ep >= max_episodes:
+                    print(
+                        "\rEpisode {:d}  Episode reward = {:.2f}   Mean loss = {:.2f}\t\t".format(
+                            ep, self.rewards, np.mean(self.update_loss)), end="")
+
+                    if ep >= self.episodes:
                         training = False
                         print('\nEpisode limit reached.')
                         break
-                    if mean_rewards >= self.reward_threshold:
-                        training = False
-                        print('\nEnvironment solved in {} episodes!'.format(
-                            ep))
-                        #break
         # save models
         self.save_models()
         # plot
         self.plot_training_rewards()
 
     def save_models(self):
-        torch.save(self.network, "Q_net")
+        torch.save(self.policy_dqn, "Q_net")
 
     def load_models(self):
-        self.network = torch.load("Q_net")
-        self.network.eval()
+        self.policy_dqn = torch.load("Q_net")
+        self.policy_dqn.eval()
 
     def plot_training_rewards(self):
         plt.plot(self.mean_training_rewards)
@@ -194,26 +196,25 @@ class TaxiDQN:
         plt.savefig('mean_training_rewards.png')
         plt.clf()
 
+
     def calculate_loss(self, batch):
         #extract info from batch
-        states, actions, rewards, dones, next_states = list(batch)
+        states, actions, rewards, dones, next_states = batch
 
         #transform in torch tensors
         rewards = torch.FloatTensor(rewards).reshape(-1, 1).to(device)
         actions = torch.LongTensor(np.array(actions)).reshape(-1, 1).to(device)
         dones = torch.IntTensor(dones).reshape(-1, 1).to(device)
-        states = from_tuple_to_tensor(states).to(device)
-        next_states = from_tuple_to_tensor(next_states).to(device)
+        states = torch.IntTensor(states).reshape(-1, 1).to(device)
+        next_states = torch.IntTensor(next_states).reshape(-1, 1).to(device)
 
         ###############
-        # DDQN Update #
+        # DQN Update #
         ###############
-        # Q(s,a) = ??
-        qvals = self.network.get_qvals(states)
+        qvals = self.policy_dqn(self.state_to_onehot(states))
         qvals = torch.gather(qvals, 1, actions)
 
-        # target Q(s,a) = ??
-        next_qvals= self.target_network.get_qvals(next_states)
+        next_qvals = self.target_dqn(self.state_to_onehot(next_states))
         next_qvals_max = torch.max(next_qvals, dim=-1)[0].reshape(-1, 1)
         target_qvals = rewards + (1 - dones)*self.gamma*next_qvals_max
 
@@ -223,33 +224,35 @@ class TaxiDQN:
         return loss
 
 
-    def update(self):
-        self.network.optimizer.zero_grad()
-        batch = self.buffer.sample_batch(batch_size=self.batch_size)
+    def update_network(self):
+        batch = self.memory.sample_batch(self.batch_size)
         loss = self.calculate_loss(batch)
-
+        
+        # Optimize the model
+        self.optimizer.zero_grad()
         loss.backward()
-        self.network.optimizer.step()
+        self.optimizer.step()
 
         self.update_loss.append(loss.item())
 
-    def initialize(self):
-        self.training_rewards = []
-        self.training_loss = []
-        self.update_loss = []
-        self.mean_training_rewards = []
-        self.sync_eps = []
-        self.rewards = 0
-        self.step_count = 0
 
     def evaluate(self, eval_env):
         done = False
         s, _ = eval_env.reset()
         rew = 0
         while not done:
-            action = self.network.greedy_action(torch.FloatTensor(s).to(device))
+            action = self.policy_dqn.greedy_action(torch.FloatTensor(s).to(device))
             s, r, terminated, truncated, _ = eval_env.step(action)
             done = terminated or truncated
             rew += r
 
         print("Evaluation cumulative reward: ", rew)
+
+
+if __name__ == '__main__':
+    agent = TaxiDQN()
+
+    agent.train()
+
+    agent.env = gym.make('Taxi-v3', render='human')
+    agent.evaluate()
